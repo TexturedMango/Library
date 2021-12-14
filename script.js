6b0bfbfb2a524c389897e87dcdb52c3660f740ac
TexturedMango <gustavoacasalg@gmail.com>
2021-12-14 18:49:05 -0400

    no idea

--- script.js
+++ script.js
@@ -14,11 +14,13 @@ let myLibrary = [
         read: false
     }
 ]
-function Book(name, author, pages, read) {
-    this.name = name;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(name, author, pages, read) {
+        this.name = name;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 function validateForm() {
     let title = document.formy.title.value;
